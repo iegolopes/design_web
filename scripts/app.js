8881fe35cec855f63f271c1c452f72e6af2ef98c
iego <iego.lopes@escolar.ifrn.edu.br>
2026-08-21 13:21:59 -0300

    adicionei novos codigos em JS

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,14 @@
 alert("olá mundo");
+
+var nasc= 2009;
+let nome = "Iego";
+const viva =true;
+let altura = 1.64;
+
+if (viva){
+    let saudacao ="Olá" + nome + "!";
+    let mensagem = `Altura${altura}m | Idade ${2026-nasc}`;
+    alert(saudacao+"\n"+mensagem);
+}else{
+    alert("Você não está vivo!")
+}
