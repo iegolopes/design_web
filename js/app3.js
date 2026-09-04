b07d3876fb3dc37632d9d6c00c2f8d1261a4f139
iego <iego.lopes@escolar.ifrn.edu.br>
2026-09-04 13:16:54 -0300

    controle de fluxo com cases

--- js/app3.js
+++ js/app3.js
@@ -1,29 +1,12 @@
 let dia = prompt("Escolha um dia da semana \n Sendo 1:Domingo - 7:Sábado")
 dia = Number(dia);
 switch(dia){
-    case 1: alert("Você escolheu domingo")
-    case 1: alert("Você escolheu domingo")
-    case 1: alert("Você escolheu domingo")
-    case 1: alert("Você escolheu domingo")
-    case 1: alert("Você escolheu domingo")
-    case 1: alert("Você escolheu domingo")
-    case 1: alert("Você escolheu domingo")
-}
-
-if (dia <= 0|| dia >=8){
-    alert("Dia invalido");
-}else if (dia==1){
-    alert("Você escolheu domingo");
-}else if (dia==2){
-    alert("Você escolheu segunda");
-}else if (dia==3){
-    alert("Você escolheu terça");
-}else if (dia==4){
-    alert("Você escolheu quarta");
-}else if (dia==5){
-    alert("Você escolheu quinta");
-}else if (dia==6){
-    alert("Você escolheu sexta");
-}else{
-    alert("Vecê escolheu sabádo")
+    case 1: alert("Você escolheu domingo"); break;
+    case 2: alert("Você escolheu segunda"); break;
+    case 3: alert("Você escolheu terça"); break;
+    case 4: alert("Você escolheu quarta"); break;
+    case 5: alert("Você escolheu quinta"); break;
+    case 6: alert("Você escolheu sexta"); break;
+    case 7: alert("Você escolheu sabádo"); break;
+    default: alert("Dia inválido"); break;
 }
