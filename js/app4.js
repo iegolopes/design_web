70d4f13718a2b23872666c7d0aec31d0fba17a34
iego <iego.lopes@escolar.ifrn.edu.br>
2026-09-04 14:26:37 -0300

    fir com novos ifs

--- js/app4.js
+++ js/app4.js
@@ -1,5 +1,13 @@
 let vezes= Number(prompt("Digite o número de vezes:"));
 for (let i = 1; i<= vezes; i++){
+    if (vezes > 100){
+        alert("Valor inválido, recarregue a página e digite um valor de 0 a 100");
+        break
+    }
     alert(`Contei ${i} vezes`);
+    if(i % 2==0){
+        continue;
+    }
+    alert(`${i} é par`);
     
 }
