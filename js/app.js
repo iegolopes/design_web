814868c8250cea8baf3ae96cee31e34e97b0d5b7
iego <iego.lopes@escolar.ifrn.edu.br>
2026-09-11 14:09:49 -0300

    alteração de imgs

--- js/app.js
+++ js/app.js
@@ -1,2 +1,13 @@
-let foto=1;
+let i=1;
 const imagem=document.getElementById("foto");
+
+function proximo(e){
+    e.preventDefault();
+    if(i<6){
+        i=i+1;
+    }
+    imagem.setAttribute("src",`./img/foto${i}.jpg`);
+}
+
+const prox=document.getElementById("prox")
+prox.addEventListener("click", proximo)
