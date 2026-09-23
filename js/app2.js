95b3ee146a028f0327bcd7c67be08834c630594b
iego <iego.lopes@escolar.ifrn.edu.br>
2026-09-23 16:59:36 -0300

    arranjo

--- js/app2.js
+++ js/app2.js
@@ -1,1 +1,11 @@
 const amigos=[]
+const cadastro =document.getElementById("cadastro");
+const nome=cadastro.nome;
+const nasc=cadastro.nasc;
+const whatsapp=cadastro.whatsapp;
+
+cadastro.addEventListener("submit", function(e){
+    e.preventDefault;
+    let item = [nome.value, nasc.value, whatsapp.value];
+    amigos.unshift(item);
+})
