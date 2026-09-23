72ac4fdbfbbf03d00383b68dcc631bdb7f5a1407
iego <iego.lopes@escolar.ifrn.edu.br>
2026-09-23 17:56:17 -0300

    remover

--- js/app2.js
+++ js/app2.js
@@ -19,10 +19,21 @@ function exibirLista(){
     let itens="";
     for(let i = 0; i<amigos.length; i++){
         let item = amigos[i]; //[Nome, Nasc, Whatsapp]
+        //Cria botao para remover
+        let remover = `<button onclick="remover(${i})">Remover</button>`
         //Cria uma tag li
-        let li = `<li>${item[0]} | ${item[1]} | ${item[2]}`;
+        let li = `<li>${item[0]} | ${item[1]} | ${item[2]} | ${remover} </li>`;
         itens = itens + li;
     }
     //Alterar o HTML da lista para ser igual aos itens
     lista.innerHTML=itens
+}
+
+function remover(i){
+    let item=amigos[i]; //[Nome, Nasc, Whatsapp]
+    let check =confirm(`Deseja realmente excluir ${item[0]}?`)
+    if(check ==true){
+        amigos.splice(i,1);//splice(posição inicial, qtd itens a remover)
+    }
+    exibirLista();
 }
