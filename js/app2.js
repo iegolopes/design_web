20f45c619c0206145ecc108261bd8a46a99ee627
iego <iego.lopes@escolar.ifrn.edu.br>
2026-09-23 17:17:33 -0300

    sddfdsfsddsf

--- js/app2.js
+++ js/app2.js
@@ -3,9 +3,26 @@ const cadastro =document.getElementById("cadastro");
 const nome=cadastro.nome;
 const nasc=cadastro.nasc;
 const whatsapp=cadastro.whatsapp;
+const lista = document.getElementById("lista")
 
 cadastro.addEventListener("submit", function(e){
     e.preventDefault;
     let item = [nome.value, nasc.value, whatsapp.value];
     amigos.unshift(item);
-})
+    //Limpa p formulario
+    cadastro.request()
+    //Atulaiza lista
+    exibirLista();
+});
+
+function exibirLista(){
+    let itens="";
+    for(let i = 0; i<amigos,amigos.length; i++){
+        let item = amigos[i]; //[Nome, Nasc, Whatsapp]
+        //Cria uma tag li
+        let li = `<li>${item[0]} | ${item[1]} | ${item[2]}`;
+        itens = itens + li;
+    }
+    //Alterar o HTML da lista para ser igual aos itens
+    lista.innerHTML=itens
+}
